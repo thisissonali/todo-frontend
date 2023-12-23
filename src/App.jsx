88ab93f4afa2d93d019d276e15171112b2aaea91
Sonali Rana <ranasonali987@gmail.com>
2023-12-23 15:41:06 +0530

    style: nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from 'react'
-import './styles/app.scss';
+import './styles/App.css'
 import { BrowserRouter as Router , Route , Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
